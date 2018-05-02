af11def905cfb15d6bf35f9b54e03e30cab52a84
LXG-Shadow <32156054+LXG-Shadow@users.noreply.github.com>
2018-05-02 19:34:33 +0800

    New

--- getword.js
+++ getword.js
@@ -28,5 +28,5 @@ function parsedict(dict){
 };
 
 function printdict(dict,dictname){
-    document.write("var " + dictname + "=" + parsedict(dict));
+    document.write("var " + dictname + "=" + parsedict(dict) + ";");
 };
