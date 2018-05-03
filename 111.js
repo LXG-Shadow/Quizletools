f547f41c4878ccd107dfe4e7e4214f0b87a018bb
LXG-Shadow <32156054+LXG-Shadow@users.noreply.github.com>
2018-05-03 10:46:11 +0800

    New

--- 111.js
+++ 111.js
@@ -102,22 +102,65 @@ function autospelling(){
 
 function autodotest(){
     var testsectionum=new Array()
-    var testsection=new Array()
     var titles = document.getElementsByTagName("h4");
     var index = 0;
     for (var i in titles){
         console.log(isNaN(parseInt(titles[i].innerText)));
-        if (! isNaN(parseInt(titles[i].innerText)))
+        if (isNaN(parseInt(titles[i].innerText)))
             break;
         testsectionum[index] = parseInt(titles[i].innerText);
         index++;
     };
-    var inputags = document.getElementsByTagName("input");
-    for (var i in inputags){
-        if 
+
+    index = 0;
+    var spantags=document.getElementsByTagName("span");
+    var questext = new Array();
+    for(var i in spantags){  
+        if(spantags[i].nodeType==1){  
+            if(spantags[i].getAttribute("class") == "TermText notranslate lang-en"){
+                questext.push(spantags[i])
+            }; 
+        };
+    };
+
+    var testsection=new Array()
+    var textareatags = document.getElementsByTagName("textarea");
+    for (var i in textareatags){
+        if(textareatags[i].nodeType==1){  
+            if(textareatags[i].getAttribute("class") == "AutoExpandTextarea-textarea"){
+                testsection.push(textareatags[i]);
+                continue;
+            };
+        }; 
+    };
+
+    while (index <testsectionum[0]) {
+        questext[index].innerText = defdict[questext[index].innerText];
+        index++;
+    };
+
+    var index = index + testsectionum[1];
+    var numofq = index + testsectionum[1];
+    while (index < numofq){
+        questext[index].innerText = defdict[questext[index].innerText];
+        index++;
+    };
+
+    var numofq = index + testsectionum[2]*5;
+    while (index < numofq){
+        questext[index].innerText = defdict[questext[index].innerText];
+        index = index + 5;
+    };
+
+    var numofq = index + testsectionum[3]*2;
+    while (index < numofq){
+        if (defdict[questext[index].innerText] == questext[index+1].innerText)
+            questext[index].innerText = "True";// + defdict[questext[index].innerText];
+        else
+            questext[index].innerText = "False";// + defdict[questext[index].innerText];
+        index = index + 2;
     };
 };
-
 
 
 
